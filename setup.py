e044d949839ffe57d2704cafc6be7a5df8b93deb
hwakabh <hrykwkbys1024@gmail.com>
2025-01-26 01:18:10 +0900

    chore: renamed Python package name with conflicts.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup # find_packages
+from setuptools import setup
 
 
 setup(
-    name="envkeeper",
-    # packages=find_packages(),
+    # Python Package name should be identical on PyPI
+    # Note that PyPI is case-insensitive
+    name="envkp",
+    version='0.0.1',
     install_requires=['setuptools'],    # httpx, requests, ...etc
     entry_points={
         "console_scripts": [
-            "envkp = envkeeper.core:cli",
-            "envkp-dump = envkeeper.core:dump",
+            # we can use as CLI with the name `envkp` & `envkp-dump`
+            "envkp = envkp.core:cli",
+            "envkp-dump = envkp.core:dump",
         ]
     }
 )
